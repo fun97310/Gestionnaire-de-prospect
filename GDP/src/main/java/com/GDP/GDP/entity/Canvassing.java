4bbb2ddfce549fd7a408c0eb170f29c0709c57f8
fun97310 <francoise.pinceau@gmail.com>
2025-10-30 21:43:25 +0100

    Add Canvassing entity

---
+++ GDP/src/main/java/com/GDP/GDP/entity/Canvassing.java
@@ -0,0 +1,10 @@
+package com.GDP.GDP.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "canvassings")
+public class Canvassing extends Prospect {
+    
+}
