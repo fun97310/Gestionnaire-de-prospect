01c56d7bebadbe43b8a13955409b0216d774242d
fun97310 <francoise.pinceau@gmail.com>
2025-10-30 21:14:33 +0100

    Add abstract class Prospect
    This class unifies the Application and Canvassing classes which have essentially the same properties, except that Application includes one additional property.

---
+++ GDP/src/main/java/com/GDP/GDP/entity/Prospect.java
@@ -0,0 +1,50 @@
+package com.GDP.GDP.entity;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.persistence.*;
+
+@MappedSuperclass
+public abstract class Prospect {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime initialApplicationDate;
+
+    private LocalDateTime dateRelaunch;
+
+    @ElementCollection
+    private List<LocalDateTime> historyOfRelaunches = new ArrayList<>();
+
+    // Getters and Setters
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public LocalDateTime getInitialApplicationDate(){
+        return initialApplicationDate;
+    }
+    public void setInitialApplicationDate(LocalDateTime initialApplicationDate){
+        this.initialApplicationDate = initialApplicationDate;   
+    }
+
+    public LocalDateTime getDateRelaunch(){
+        return dateRelaunch;
+    }
+
+    public void setDateRelaunch(LocalDateTime dateRelaunch){
+        this.dateRelaunch = dateRelaunch;
+    }
+
+    public List<LocalDateTime> getHistoryOfRelaunches(){
+        return historyOfRelaunches;
+    }
+
+    public void setHistoryOfRelaunches(List<LocalDateTime> historyOfRelaunches){
+        this.historyOfRelaunches = historyOfRelaunches;
+    }
+}
